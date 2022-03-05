56613c66a7ea93c5f4079a137a4cb5c6ce92640b
ParthGoyal08 <69162910+ParthGoyal08@users.noreply.github.com>
2022-03-05 14:27:53 -0500

    Add files via upload

---
+++ Navigation/TabNavigator.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "./screens/Feed";
+import CreateStory from "./screens/CreateStory";
+
+const Tab = createBottomTabNavigator();
+const BottomTabNavigator = ()=>{
+    return (
+      <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Create Story") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={RFValue(25)}
+              color={color}
+              style={styles.icons}
+            />
+          );
+        }
+      })}
+      activeColor={"#ee8249"}
+      inactiveColor={"gray"}
+    >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Create Story" component={CreateStory} />
+    </Tab.Navigator>
+      );
+}
+
+export default BottomTabNavigator;
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAwI7qeOO7eEr-bEDQTaqPpo1lj1YDIawA",
+    authDomain: "spectagram-app-a6344.firebaseapp.com",
+    projectId: "spectagram-app-a6344",
+    storageBucket: "spectagram-app-a6344.appspot.com",
+    messagingSenderId: "1000658722587",
+    appId: "1:1000658722587:web:5d8325a9b711e7d063fdbe"
+  };
